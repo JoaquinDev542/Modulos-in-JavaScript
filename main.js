b6b1898158b9f05831cb7334b32a114ae413cd30
Joaquín Uriel García <82168815+JoaquinDev542@users.noreply.github.com>
2022-02-22 21:14:08 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import saludar , {userName , age} from "./constantes.js";
+
+saludar("HOLA");
+// HOLA
+
+console.log(`Hola, mi nombre es ${userName} y tengo ${age} años.`);
+// Hola, mi nombre es Joaquin y tengo 19 años.
